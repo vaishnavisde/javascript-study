40d45d397097c3f267e3c3c429d805d936799ac6
Vaishnavi <vaishnavikumarasamy5@gmail.com>
2021-11-13 13:12:07 +0000

    add some code

---
+++ code-quality.js
@@ -0,0 +1,7 @@
+//DEBUGGING IN BROWSER
+//debugging is the process of finding and fixing error
+
+
+//BREAKPOINT
+//a breakpoint is a point of code where the debugger will automatically pause the JS execution
+//we can also pause the code by using debugger command
