3ba2244a7a086e0cd530ac5824db43a1990ef415
Vaishnavi <vaishnavikumarasamy5@gmail.com>
2021-12-20 20:15:35 +0000

    add some code

---
+++ object-basic/constructor-factory.js
@@ -0,0 +1,28 @@
+//there is two way to create new object
+//constructor function and factory function
+
+
+//constructor function should be in pascal scale
+//here we using the "new" keyword
+function Person(name) {
+  this.name = name;
+  this.greetings = Function();
+  {
+    console.log("my name is" + " " + name);
+  }
+}
+let user = new Person("arun");
+user.greetings();
+
+//factory function
+
+function createPerson(name) {
+  return {
+    name,
+    greetings() {
+      console.log(`my name is ${this.name}`);
+    },
+  };
+}
+let kirthick = createPerson("kirthick");
+kirthick.greetings();
