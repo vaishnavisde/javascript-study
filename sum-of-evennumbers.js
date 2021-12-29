ecaf78dd56317af5812f7df231f4eda1f76a229f
Vaishnavi <vaishnavikumarasamy5@gmail.com>
2021-12-29 12:32:28 +0530

    add files

--- sum-of-evennumbers.js
+++ sum-of-evennumbers.js
@@ -1,10 +1,12 @@
 let num = 10;
 
-let sum=0;
-for(i=1;i<=num;i++){
-  if(i%2==0){
-    sum=sum+i;
+let sum = 0;
+for (i = 1; i <= num; i++) {
+  if (i % 2 == 0) {
+    sum = sum + i;
   }
   
 }
-console.log( "sum of even number"+" "+sum);
+console.log("sum of even number:" + " " + sum);
+
+
